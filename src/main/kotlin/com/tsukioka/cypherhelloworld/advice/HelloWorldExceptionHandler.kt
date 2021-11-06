2a60a4b1a7ba85d579e21d9903e3e5cd5213c717
akito-tsukioka <akito.tsukioka@askul.com>
2021-11-06 20:47:36 +0900

    task-03 ValidationException追加

--- src/main/kotlin/com/tsukioka/cypherhelloworld/advice/HelloWorldExceptionHandler.kt
+++ src/main/kotlin/com/tsukioka/cypherhelloworld/advice/HelloWorldExceptionHandler.kt
@@ -7,12 +7,17 @@ import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestControllerAdvice
 import org.springframework.web.servlet.NoHandlerFoundException
+import javax.validation.ValidationException
 
 @RestControllerAdvice
 class HelloWorldExceptionHandler {
     @ExceptionHandler
     @ResponseStatus(HttpStatus.NOT_FOUND)
     fun handler(ex: NoHandlerFoundException): ErrorResponse = ErrorResponse(ErrorType.NOT_FOUND.reason)
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    fun handler(ex: ValidationException): ErrorResponse = ErrorResponse(ErrorType.NAME_ERROR.reason)
 
     @ExceptionHandler
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
--- src/main/kotlin/com/tsukioka/cypherhelloworld/entity/ErrorType.kt
+++ src/main/kotlin/com/tsukioka/cypherhelloworld/entity/ErrorType.kt
@@ -4,5 +4,6 @@ enum class ErrorType(
     val reason: String
 ) {
     NOT_FOUND("no handler found"),
-    INTERNAL_SERVER_ERROR("something wrong ;-(")
+    INTERNAL_SERVER_ERROR("something wrong ;-("),
+    NAME_ERROR("invalid parameter: detail: [HelloWorld.name: 3 から 10 の間のサイズにしてください]")
 }
